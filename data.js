1e922559289306915f7206b439f1456a7f3504be
Jevousli <nashwaazp@gmail.com>
2023-06-14 17:07:04 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,305 @@
+let songs = [
+  {
+    name: "Only Angel",
+    path: "assets/musics/angels 1.mp3",
+    artist: "Kahfi Reihan",
+    cover: "assets/images/angel.jpg",
+  },
+  {
+    name: "0.52",
+    path: "assets/musics/Suara 052.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/sel.jpg",
+  },
+  {
+    name: "0.54",
+    path: "assets/musics/Suara 054.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/sel.jpg",
+  },
+  {
+    name: "0.91",
+    path: "assets/musics/Suara 0.91.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/sel.jpg",
+  },
+  {
+    name: "103",
+    path: "assets/musics/Suara 103.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/sel.jpg",
+  },
+  {
+    name: "0.80",
+    path: "assets/musics/Suara 080.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/sel.jpg",
+  },
+  {
+    name: "0.67",
+    path: "assets/musics/Suara 067.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/sel.jpg",
+  },
+  {
+    name: "0.84",
+    path: "assets/musics/Suara 084.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/sel.jpg",
+  },
+];
+// the future
+let sings = [
+  {
+    name: "Egyptian Queen",
+    path: "assets/musics/tears in the mirror 1.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/wom.jpg",
+  },
+  {
+    name: "0.68",
+    path: "assets/musics/Suara 068.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/mus.jpg",
+  },
+  {
+    name: "Queen (Riff)",
+    path: "assets/musics/queen.mp3",
+    artist: "Kahfi Reihan",
+    cover: "assets/images/mus.jpg",
+  },
+  {
+    name: "0.55",
+    path: "assets/musics/Suara 055.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/mus.jpg",
+  },
+  {
+    name: "0.92",
+    path: "assets/musics/Suara 092.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/mus.jpg",
+  },
+  {
+    name: "0.94",
+    path: "assets/musics/Suara 094.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/mus.jpg",
+  },
+];
+
+// echoesssssssssssssss
+let sing = [
+  {
+    name: "Nothing",
+    path: "assets/musics/Nothing.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/nothing.jpg",
+  },
+  {
+    name: "Lost",
+    path: "assets/musics/lost.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/play.jpg",
+  },
+  {
+    name: "It's You",
+    path: "assets/musics/It's You.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/yuu.jpg",
+  },
+  {
+    name: "Hello",
+    path: "assets/musics/Hello.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/hello.jpg",
+  },
+  {
+    name: "Sudah",
+    path: "assets/musics/Sudah.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/sudah.jpg",
+  },
+];
+
+// cLESTIAALLLL
+let singe = [
+  {
+    name: "Teman Perjalanan",
+    path: "assets/musics/Teman.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/teman.jpg",
+  },
+  {
+    name: "Kenangan Manis",
+    path: "assets/musics/Kenangan Manis.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/kenangab.jpg",
+  },
+  {
+    name: "Don't Look Back At Anger",
+    path: "assets/musics/Don't Look Back At Anger.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/dont.jpg",
+  },
+  {
+    name: "A Day That Feels Better",
+    path: "assets/musics/A Day That Feels Better.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/birdy.jpg",
+  },
+  {
+    name: "Officialy Missing You",
+    path: "assets/musics/Officialy Missing You.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/off.jpg",
+  },
+];
+
+// brokenboyy
+let singo = [
+  {
+    name: "The most beautiful thing",
+    path: "assets/musics/The most beautiful thing.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/nothing.jpg",
+  },
+  {
+    name: "Little Freak",
+    path: "assets/musics/Little Freak.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/little.jpg",
+  },
+  {
+    name: "Purple Sigh",
+    path: "assets/musics/Purple Sigh.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/birdy.jpg",
+  },
+  {
+    name: "intentions",
+    path: "assets/musics/Intentions.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/hello.jpg",
+  },
+  {
+    name: "I Got To Get You",
+    path: "assets/musics/getyousad.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/birdy.jpg",
+  },
+];
+// meeeeeeeeee 071 am
+let singu = [
+  {
+    name: "Untitled",
+    path: "assets/musics/Untitled.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/uti.jpg",
+  },
+  {
+    name: "One Only",
+    path: "assets/musics/One and Only.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/kenangab.jpg",
+  },
+  {
+    name: "Softly Spoken Love",
+    path: "assets/musics/Softly Spoken Love.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/sof.jpg",
+  },
+  {
+    name: "Never On the Day You Leave",
+    path: "assets/musics/Never On the Day You Leave.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/never.jpg",
+  },
+  {
+    name: "I Can't Stop Loving You",
+    path: "assets/musics/I Can't Stop Loving You.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/cant.jpg",
+  },
+];
+// jazz
+let singa = [
+  {
+    name: "It's Man's World",
+    path: "assets/musics/itsman.mp3",
+    artist: "James brown ",
+    cover: "assets/images/james.jpg",
+  },
+  {
+    name: "Just The Two of Us",
+    path: "assets/musics/two.mp3",
+    artist: "Grover washington ",
+    cover: "assets/images/just.jpg",
+  },
+  {
+    name: "Video Games",
+    path: "assets/musics/Videogames.mp3",
+    artist: "gracia abraham",
+    cover: "assets/images/gracia.jpg",
+  },
+  {
+    name: "The Longest Time",
+    path: "assets/musics/thelongest.mp3",
+    artist: "billy joel",
+    cover: "assets/images/billy.jpg",
+  },
+  {
+    name: "It's Been A Long Time",
+    path: "assets/musics/longtime.mp3",
+    artist: "bing cosby",
+    cover: "assets/images/bing.jpg",
+  },
+];
+// solo
+let singi = [
+  {
+    name: "Benci tuk mencinta",
+    path: "assets/musics/benci.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/benci.jpg",
+  },
+  {
+    name: "Bold As Love",
+    path: "assets/musics/bold.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/bold.jpg",
+  },
+  {
+    name: "I dont trust my self with loving you",
+    path: "assets/musics/trust.mp3",
+    artist: "Kahfi Reihan ",
+    cover: "assets/images/bold.jpg",
+  },
+];
+// cheriee
+let singp = [
+  {
+    name: "The great war",
+    path: "assets/musics/tgw.mp3",
+    artist: "Taylor swift ",
+    cover: "assets/images/tay.jpg",
+  },
+  {
+    name: "valentine",
+    path: "assets/musics/tine.mp3",
+    artist: " laufey",
+    cover: "assets/images/valen.jpg",
+  },
+  {
+    name: "k.",
+    path: "assets/musics/ka.mp3",
+    artist: " ciggarets after sex",
+    cover: "assets/images/cig.jpg",
+  },
+  {
+    name: "Coney island",
+    path: "assets/musics/coney.mp3",
+    artist: " taylor swift",
+    cover: "assets/images/ever.jpg",
+  },
+];
